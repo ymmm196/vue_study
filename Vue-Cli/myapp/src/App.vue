902d47dd339384a3cb41ce219de69856980352c1
ymmm196 <yangming196@126.com>
2019-09-30 09:35:46 +0800

    0930

--- Vue-Cli/myapp/src/App.vue
+++ Vue-Cli/myapp/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
-    <TodoList />
+    <Tab />
   </div>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
 import TodoList from "./components/TodoList.vue";
+import Tab from "./components/Tab.vue";
+import "bootstrap/dist/css/bootstrap.css";
 export default {
   name: "app",
   components: {
     // HelloWorld,
-    TodoList
+    // TodoList
+    Tab
   }
 };
 </script>
---
+++ Vue-Cli/myapp/src/components/Tab.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul class="nav nav-pills">
+      <li
+        :key="item"
+        role="presentation"
+        v-for="(item,idx) in tabs"
+        v-bind:class="{active:idx===currentIdx}"
+        v-on:click="changeTab(idx)"
+      >
+        <a href="#">{{item.name}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tabs: ["体育", "娱乐", "新闻", "视频"],
+      currentTab: ""
+    };
+  }
+};
+</script>
+<style>
+</style>
